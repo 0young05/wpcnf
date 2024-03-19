ebf13a3a7a31aa98852d8af13366ab6399d970c3
0young05 <jinyeongan89@gmail.com>
2024-03-20 00:06:12 +0900

    1st rhkwp

---
+++ 202410863_안진영.py
@@ -0,0 +1,10 @@
+def get_radius():
+    r=int(input())
+    return r
+print("넓이를 구하고자 하는 원의 반지름은?")
+r=get_radius()
+def get_circle_area():
+    print(3.14*r*r)
+    return 
+print("반지름 4인 원의 넓이 = 3.14*4*4 = ",end="")
+result=get_circle_area()
